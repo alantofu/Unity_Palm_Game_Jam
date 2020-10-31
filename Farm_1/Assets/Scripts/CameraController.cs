895ce8fe7ed7e981fe6fb50611ec296f30d4e931
TINGWEIJING <tingweijingting2000@gmail.com>
2020-10-31 15:53:40 +0800

    Update script

--- Farm_1/Assets/Scripts/CameraController.cs
+++ Farm_1/Assets/Scripts/CameraController.cs
@@ -14,18 +14,54 @@ public class CameraController : MonoBehaviour
     public float movementTime; // change camera smoothness movement
     public Vector3 newPosition;
     public Vector3 zoomAmount;
+    public Vector3 newZoom;
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
-
+        newZoom = cameraTransform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleMovementInput();
+        HandleMouseInput();
+    }
+
+    void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
     }
 
     void HandleMovementInput()
