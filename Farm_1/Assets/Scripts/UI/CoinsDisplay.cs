bfdd38ba7dc74720746ec1fe6864d89f3734232a
HeLinChooi <helinchooi@gmail.com>
2020-11-07 15:44:40 +0800

    player script added

---
+++ Farm_1/Assets/Scripts/UI/CoinsDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CoinsDisplay : MonoBehaviour
+{
+  public Text coinText;
+  private int money = 10;
+  // public Player player;
+  void Start()
+  {
+    // coinText.text = "Coins: " + money;
+    coinText.text = "Coins: " + Player.Instance.Money;
+    // player = GetComponent<Player>();
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    // coinText.text = "Coins: " + money;
+    coinText.text = "Coins: " + Player.Instance.Money;
+    // coinText.text = "Coins: " + Player.Money;
+    if (Input.GetMouseButtonDown(0))
+    {
+      Debug.Log("mouse down");
+      // money--;
+      Player.Instance.reduceMoney(5);
+    }
+  }
+}
---
+++ Farm_1/Assets/Scripts/UI/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Player
+{
+  private static Player instance = null;
+  private static readonly object padlock = new object();
+  private int money = 100;
+
+  Player()
+  {
+  }
+
+  public static Player Instance
+  {
+    get
+    {
+      lock (padlock)
+      {
+        if (instance == null)
+        {
+          instance = new Player();
+        }
+        return instance;
+      }
+    }
+  }
+
+  public int Money
+  {
+    get { return money; }
+    set { money = value; }
+  }
+  public int reduceMoney(int amount)
+  {
+    int currAmount = Money;
+    currAmount -= amount;
+    Money = currAmount;
+    return currAmount;
+  }
+}
