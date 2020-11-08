f48fb7f9c6343cc1adbcb6be85f3ba17e45e494b
HeLinChooi <helinchooi@gmail.com>
2020-11-08 22:40:15 +0800

    addProductNum bug remain

--- Farm_1/Assets/Scripts/HelinScripts/RepDisplay.cs
+++ Farm_1/Assets/Scripts/HelinScripts/RepDisplay.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 public class RepDisplay : MonoBehaviour
 {
-  public Text text;
+  public TMP_Text text;
   public Player player;
   void Start()
   {
