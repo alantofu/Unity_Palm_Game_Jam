a5e0cd18bbff674c8e5b8527943c1f7fa4410d9c
Yong Ming <yongming1124@gmail.com>
2020-11-10 23:45:10 +0800

    modify UI

---
+++ Farm_1/Assets/Scripts/UI/SwitchPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchPanel : MonoBehaviour
+{
+    public GameObject[] Panel;
+
+    public void switchPanel(){
+        if(Panel[0] != null && Panel[1]!=null){
+            bool isActive = Panel[0].activeSelf;
+            Panel[0].SetActive(!isActive);
+            isActive = Panel[1].activeSelf;
+            Panel[1].SetActive(!isActive);
+        }
+    }
+}
