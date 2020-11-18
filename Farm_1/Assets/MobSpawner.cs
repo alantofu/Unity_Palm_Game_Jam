50ff3779daa93bf63be13b4f52c7a5faaff5eb16
Alantofu <alantofu99@gmail.com>
2020-11-19 01:54:27 +0800

    mobspawner

---
+++ Farm_1/Assets/MobSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public GameObject WorkerPrefab;
+    public Transform WorkerParent;
+    private GridSystem gridSystem;
+    public int WorkerAmount;
+
+    void Start()
+    {
+        gridSystem = GridSystem.Instance;
+        SpawnWorkers();
+    }
+
+    private void SpawnWorkers()
+    {
+        for (int i = 0; i < WorkerAmount; i++)
+        {
+            int RanX = Random.Range(0, gridSystem.objectOnGrid.GetUpperBound(0));
+            int RanZ = Random.Range(0, gridSystem.objectOnGrid.GetUpperBound(1));
+            GameObject tempObj = Instantiate(WorkerPrefab,
+                                                gridSystem.getPositionByGridPoint(RanX, RanZ),
+                                                Quaternion.identity,
+                                                WorkerParent);
+            tempObj.name = "Worker (" + RanX.ToString() + ", " + RanZ.ToString() + ")";
+            gridSystem.objectOnGrid[RanX, RanZ] = tempObj;
+        }
+    }
+}
