47151f67f9df94543554bd4f474f018dbf9e978e
Alantofu <alantofu99@gmail.com>
2020-11-19 20:13:31 +0800

    more sound

--- Farm_1/Assets/playaudio.cs
+++ Farm_1/Assets/playaudio.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class playaudio : MonoBehaviour
 {
     public AudioSource building;
+    public AudioSource exit;
+    public AudioSource market;
+    public AudioSource leaderboard;
+    public AudioSource news;
     void Start()
     {
        
     }
-    public void playBuilding()
+    public void playbuilding()
     {
         building.Play();
+    }
+
+    public void playexit()
+    {
+        exit.Play();
+    }
+    public void playmarket()
+    {
+        market.Play();
+    }
+    public void playleaderboard()
+    {
+        leaderboard.Play();
+    }
+    public void playnews()
+    {
+        news.Play();
     }
 }
