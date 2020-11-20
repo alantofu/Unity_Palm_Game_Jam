64747b17fa15a37fbbc369746e7f0655f24695f3
TINGWEIJING <tingweijingting2000@gmail.com>
2020-11-21 07:25:17 +0800

    No save file

--- Farm_1/Assets/StartScene.cs
+++ Farm_1/Assets/StartScene.cs
@@ -7,16 +7,16 @@ public class StartScene : MonoBehaviour
 {
   public void SceneLoader()
   {
-    if (PlayerPrefs.GetInt("FirstLaunch") == 0)
-    {
+    // if (PlayerPrefs.GetInt("FirstLaunch") == 0)
+    // {
       //First launch
-      PlayerPrefs.SetInt("FirstLaunch", 1);
+      // PlayerPrefs.SetInt("FirstLaunch", 1);
       SceneManager.LoadScene(1);
-    }
-    else
-    {
-      //Load scene_02 if its not the first launch
-      SceneManager.LoadScene(2);
-    }
+    // }
+    // else
+    // {
+    //   //Load scene_02 if its not the first launch
+    //   SceneManager.LoadScene(2);
+    // }
   }
 }
